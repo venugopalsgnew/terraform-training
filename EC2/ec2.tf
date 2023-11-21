700be6810ea819d03ac552b6d4e7c7e767b1c1d5
Venu <Venugopal.aix@gmail.com>
2023-11-21 09:22:18 +0530

    firt EC2 Code

---
+++ EC2/ec2.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.26.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+
+}
+
+resource "aws_instance" "terraform_created" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.large"
+
+  tags = {
+    Name = "HelloWorld-2"
+  }
+}
+
