2fe6fe0c9e4a4c7341e42cff5b1b804b4ef21fa5
venugopalsgnew <venugopal.aix@gmail.com>
2024-09-18 07:01:26 +0530

    Create list_variable_demo.tf

---
+++ Variables_Introduce_Ec2/list_variable_demo.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI
+  instance_type = "t2.micro"
+
+  # Using element function to get the first availability zone
+  availability_zone = element(var.availability_zones, 0)
+  # availability_zone = var.availability_zones[0]  # First availability zone
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
+
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]  # Example AZs
+}
+
+
+
