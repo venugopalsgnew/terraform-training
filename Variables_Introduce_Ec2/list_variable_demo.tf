98bb8f8021b4149d616058a8ee00a60b750b6700
venugopalsgnew <venugopal.aix@gmail.com>
2025-11-12 13:09:06 +0530

    Introduce availability_zones variable for EC2 instances
    Added a variable for availability zones to create instances in each zone.

--- Variables_Introduce_Ec2/list_variable_demo.tf
+++ Variables_Introduce_Ec2/list_variable_demo.tf
@@ -18,5 +18,26 @@ variable "availability_zones" {
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]  # Example AZs
 }
 
+##############################################################33
+
+To create Instance in every available zone
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+resource "aws_instance" "example" {
+  count = length(var.availability_zones)
+
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "MyInstance-${var.availability_zones[count.index]}"
+  }
+}
 
 
