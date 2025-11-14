59981ba4d8d3afb4632bfb6303431c8cc4e2056e
venugopalsgnew <venugopal.aix@gmail.com>
2025-11-14 14:47:35 +0530

    Create SG_default_vpc.tf for security group setup
    This file defines a security group for allowing TLS and SSH access to example application servers in the default VPC.

---
+++ Terraform_Assignments/SG_default_vpc.tf
@@ -0,0 +1,31 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "example-sg"
+  description = "Security group for example App Servers"
+  vpc_id      = data.aws_vpc.default.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv41" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
